724e033288f812b6261d3a2bb0b33847e2f6fa62
allankosciuszko <bartosz.ratajczyk@gmail.com>
2019-06-07 23:53:15 +0200

    Update warehauses_bakaries_optimal_routes.r
    fix - missing code

--- warehauses_bakaries_optimal_routes.r
+++ warehauses_bakaries_optimal_routes.r
@@ -1,17 +1,38 @@
-install.packages('binhf');
-library(binhf);
+#install.packages('binhf');
+library(binhf)
 
-costs = c(50,40,50,20,40,80,70,30,60,40,70,80,40,60,50,50);
-
-warhause_1 = c(rep(1,4), rep(0,8));
-warhause_2 = shift(mag_1, 4);
-warhause_3 = shift(mag_1, 2 *4);
+library(boot)
 
 
-bakery_1 = rep(c(0,0,0,1), 4)
+route_costs = c(50, 40, 50, 20,
+                40, 80, 70, 30,
+                60, 40, 70, 80)
+
+
+requirements_bakeries = c(40, 60, 50, 50)
+
+constraints_warehouses = c(700, 500, 800)
+
+warehouse_1 = c(rep(1, 4), rep(0, 8))
+warehouse_2 = shift(warehouse_1, 4)
+warehouse_3 = shift(warehouse_1, 2 * 4)
+
+bakery_1 = rep(c(0, 0, 0, 1), 3)
 bakery_2 = shift(bakery_1, 1)
 bakery_3 = shift(bakery_1, 2)
 bakery_4 = shift(bakery_1, 3)
+
+
+
+simplex(
+  a = route_costs,
+  A2 =  rbind(warehouse_1, warehouse_2, warehouse_3),
+  b2 = constraints_warehouses,
+  A1  = rbind(bakery_1, bakery_2, bakery_3, bakery_4),
+  b1 = requirements_bakeries,
+  maxi = F
+)
+
 
 
 
